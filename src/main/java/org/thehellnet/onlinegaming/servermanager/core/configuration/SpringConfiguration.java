e81c8cf876ecf911701689526ac081a43c797ff5
sardylan <sardylan@gmail.com>
2019-01-10 19:45:50 +0100

    Adds configuration classes and logback config

---
+++ src/main/java/org/thehellnet/onlinegaming/servermanager/core/configuration/SpringConfiguration.java
@@ -0,0 +1,33 @@
+package org.thehellnet.onlinegaming.servermanager.core.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+@Configuration
+@ComponentScan(basePackages = "org.thehellnet.onlinegaming.servermanager.core")
+@EnableWebMvc
+public class SpringConfiguration implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setServletContext(servletContext);
+        context.setConfigLocation("org.thehellnet.onlinegaming.servermanager.core.configuration");
+        context.refresh();
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(new GenericWebApplicationContext());
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("servlet", dispatcherServlet);
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/*");
+    }
+}
